52008be342522985988ec476d5189dc9d0a32596
Chris Kim <hjchristopher.dev@gmail.com>
2024-02-10 14:40:56 -0800

    Add permission request feature for gallery button

--- app/src/main/java/eu/tutorials/drawingapp/MainActivity.kt
+++ app/src/main/java/eu/tutorials/drawingapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package eu.tutorials.drawingapp
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +9,9 @@ import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
@@ -14,6 +19,30 @@ class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
+
+    val requestPermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                val permissionName = it.key
+                val isGranted = it.value
+                if (isGranted) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Permission granted. Now you can read the storage files.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    if (permissionName==Manifest.permission.MANAGE_EXTERNAL_STORAGE) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Permission denied.",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,6 +59,11 @@ class MainActivity : AppCompatActivity() {
         val ib_brush : ImageButton = findViewById(R.id.ib_brush)
         ib_brush.setOnClickListener {
             showBrushSizeChooserDialog()
+        }
+
+        val ibGallery : ImageButton = findViewById(R.id.ib_gallery)
+        ibGallery.setOnClickListener {
+            requestStoragePermission()
         }
     }
 
@@ -71,5 +105,26 @@ class MainActivity : AppCompatActivity() {
 
             mImageButtonCurrentPaint = view
         }
+    }
+
+    private fun requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_MEDIA_IMAGES)
+        ) {
+            showRationaleDialog("Kids Drawing App", "Kids Drawing App " + "needs to access your external storage.")
+        } else {
+            requestPermission.launch(arrayOf(Manifest.permission.READ_MEDIA_IMAGES))
+        }
+    }
+
+    private fun showRationaleDialog(title: String, message: String) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
     }
 }
